daeb7cc8039eccc44d8d212e52a705fea71b6a05
Suhas Karun Yallamati <suhaskarun8696@gmail.com>
2026-08-22 21:12:29 +0530

    Time: 10 ms (54.27%), Space: 13.5 MB (97.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        for i in dic:
+            if dic[i] > (len(nums)//2):
+                return i
