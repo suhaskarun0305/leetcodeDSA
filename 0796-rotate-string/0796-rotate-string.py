76cf22fee8e0a0362dab70f7ae57c3b0ad818523
Suhas Karun Yallamati <suhaskarun8696@gmail.com>
2026-08-22 16:01:06 +0530

    Time: 0 ms (100%), Space: 12.3 MB (88.79%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        x = s+s
+         
+        if len(s) != len(goal):
+            return False
+
+        if goal in x:
+            return True
+        else:
+            return False
