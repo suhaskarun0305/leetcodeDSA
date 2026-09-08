fcb8bffbd7fa7f490e603d2d6356b08bfc09f113
Suhas Karun Yallamati <suhaskarun8696@gmail.com>
2026-09-08 10:16:23 +0530

    Time: 266 ms (49.23%), Space: 16.6 MB (99.92%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        
+        dic = {}
+
+        r = l = 0
+        count = 0
+        max1 = 0
+
+        while r < len(fruits):
+            if fruits[r] not in dic:
+                dic[fruits[r]] = 1
+            else:
+                dic[fruits[r]] += 1
+             
+            while len(dic) > 2:
+                if dic[fruits[l]] == 1:
+                    dic.pop(fruits[l])
+                else:
+                    dic[fruits[l]] -= 1
+                l += 1
+            
+            count = r - l + 1
+            
+            max1 = max(max1,count)
+            r += 1
+        return max1
+                
